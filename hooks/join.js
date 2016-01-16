9fd32ec44e137a8f035ed46ff9f3d1559df2708b
ansuz <ansuz@transitiontech.ca>
2016-01-16 17:01:52 -0500

    add more trusted hosts

--- hooks/join.js
+++ hooks/join.js
@@ -17,6 +17,10 @@ var ips = [
     'fc92:8136:dc1f:e6e0:4ef6:a6dd:7187:b85f', // cjd?
     'fc67:9816:2ccc:c4c2:f76c:1d09:a7a5:44e', // larsg
     'fca2:ef7:4f84:3999:6db0:1a0f:c80d:3912', // emery
+    'fce4:ebad:b910:f379:dc52:2fa5:1538:c860', // whyruslee
+    'fcc3:def:490f:149b:5b07:9952:d47c:6ec1', // dvn
+    'fc9e:ab5a:c263:35c9:8156:8b4b:4850:c6da', // woshilapi
+    'fc05:7988:6e81:6986:9410:8e06:b2b8:8bd3', // ralph
 ];
 
 if (ips.indexOf(msg.host) !== -1) {
