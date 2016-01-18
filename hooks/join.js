f6a65d674e9fe521c700cb0b812f08e467d3408a
ansuz <ansuz@transitiontech.ca>
2016-01-18 03:54:54 -0500

    add museum to op list

--- hooks/join.js
+++ hooks/join.js
@@ -13,6 +13,7 @@ var ips = [
     'fc34:8675:ed95:600c:38d7:6eb8:f5b9:5bfa', // queen
     'fcbf:8145:9f55:202:908f:bcce:c01e:caf2', // dundas
     'fc27:520a:25a3:60c4:be42:3d86:aab5:4307', // bloor
+    'fcbe:5f12:67d8:77ea:e4d8:aecc:2b4f:a4b', // museum
 
     'fc92:8136:dc1f:e6e0:4ef6:a6dd:7187:b85f', // cjd?
     'fc67:9816:2ccc:c4c2:f76c:1d09:a7a5:44e', // larsg
