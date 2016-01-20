1e47052abdd8bfd34f410e66d46924be84b5493e
ansuz <ansuz@transitiontech.ca>
2016-01-20 09:17:23 -0500

    try again

--- hooks/message.js
+++ hooks/message.js
@@ -94,7 +94,7 @@ function validPercent (token) {
 function nick2Host (nick, cb) {
     // cb(/*ERROR*/, /*result*/);
     bot.whois(nick, function (message) {
-        if (message && message.info && message.info.host)) {
+        if (message && message.info && message.info.host) {
             cb(null, message.info.host);
         } else {
             cb("could not find a host for that nick", null);
