342099a32415cfa7dcd34427f1b8dc621e2ee28e
Caleb James DeLisle <cjd@cjdns.fr>
2016-02-09 23:36:51 +0100

    don't dump everything in the console

--- check.sh
+++ check.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ps -ef | grep -v grep | grep -q 'node ./httpd.js' || node ./httpd.js &
-ps -ef | grep -v grep | grep -q 'node ./index.js' || node ./index.js &
+ps -ef | grep -v grep | grep -q 'node ./httpd.js' || node ./httpd.js 2>&1 >>./https.log &
+ps -ef | grep -v grep | grep -q 'node ./index.js' || node ./index.js 2>&1 >>./bot.log &
