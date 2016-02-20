bf9554301c214a81401e24510661f42abb23ffef
Caleb James DeLisle <cjd@cjdns.fr>
2016-02-21 00:19:08 +0100

    Added a few more functions

--- karma.js
+++ karma.js
@@ -1,3 +1,5 @@
+;(function () { 'use strict';
+var Humans = require('./Humans');
 var ROOT = "fc7f:3d04:419f:e9b0:526f:fc6a:576:cba0";
 var TRAIN_UNTIL = 1453844364189;
 var RESERVE_K = 0.3;
@@ -97,24 +99,132 @@ var run = function (trusts) {
     return zkarmas;
 };
 
+// Pure Fractional reserve.
+var runRootless0 = function (state0, trusts) {
+    var state = JSON.parse(JSON.stringify(state0));
+    trusts = dedupe(trusts);
+
+    for (let addr in state) {
+        let n = state[addr];
+        n.totalOutgoingTrust = 0;
+        n.availableKarma = n.karma;
+        n.karma = 0;
+        n.nextAvailableKarma = 0;
+    }
+    let highestTotalTrust = 0;
+    trusts.forEach(function (t) {
+        let n = state[t.src];
+        n.totalOutgoingTrust += t.trust;
+        if (n.totalOutgoingTrust > highestTotalTrust) { highestTotalTrust = n.totalOutgoingTrust; }
+    });
+
+    var cycles = 0;
+    var cycle = function () {
+        cycles++;
+        trusts.forEach(function (t) {
+            var src = state[t.src];
+            var dst = state[t.dest];
+            //if (!dst) { return; }
+            var tfrac = t.trust / highestTotalTrust;
+            if (tfrac > 1) { throw new Error(); }
+            dst.nextAvailableKarma +=
+                (src.availableKarma - (src.availableKarma * RESERVE_K)) * tfrac;
+        });
+        let totalDiff = 0;
+        let totalKarma = 0;
+        for (let addr in state) {
+            let n = state[addr];
+            totalDiff += n.nextAvailableKarma;
+            n.karma += n.availableKarma;
+            totalKarma += n.karma;
+            n.availableKarma = n.nextAvailableKarma;
+            n.nextAvailableKarma = 0;
+        }
+        console.log(totalKarma + '  ' + totalDiff + '  ' + cycles);
+        return totalDiff * (1000 / totalKarma);
+    };
+    while (cycle() > 0.01) {}
+    for (let addr in state) {
+        let n = state[addr];
+        n.karma += n.availableKarma;
+        delete n.availableKarma;
+        delete n.nextAvailableKarma;
+        delete n.totalOutgoingTrust;
+    }
+    console.log(cycles + ' cycles');
+    return state;
+};
+
+var runRootless1 = function (state0, trusts) {
+    var state = JSON.parse(JSON.stringify(state0));
+    trusts = dedupe(trusts);
+
+    for (let addr in state) {
+        let n = state[addr];
+        n.totalOutgoingTrust = 0;
+        n.nextKarma = n.karma;
+    }
+    let highestTotalTrust = 0;
+    trusts.forEach(function (t) {
+        let n = state[t.src];
+        n.totalOutgoingTrust += t.trust;
+        if (n.totalOutgoingTrust > highestTotalTrust) { highestTotalTrust = n.totalOutgoingTrust; }
+    });
+
+    var cycles = 0;
+    var cycle = function () {
+        cycles++;
+        trusts.forEach(function (t) {
+            var src = state[t.src];
+            var dst = state[t.dest];
+            var tfrac = t.trust / highestTotalTrust;
+            if (tfrac > 1) { throw new Error(); }
+            dst.nextKarma += src.karma * tfrac;
+        });
+        let totalDiff = 0;
+        let totalKarma = 0;
+        for (let addr in state) { totalKarma += state[addr].nextKarma; }
+        let multiplier = 1000/totalKarma;
+        for (let addr in state) {
+            let n = state[addr];
+            let nc = n.nextKarma * multiplier;
+            totalDiff += Math.abs(nc - n.karma);
+            n.nextKarma = n.karma = nc;
+        }
+        console.log(totalKarma + '  ' + totalDiff + '  ' + cycles);
+        return totalDiff;
+    };
+    while (cycle() > 1) {}
+    for (let addr in state) {
+        let n = state[addr];
+        delete n.nextKarma;
+        delete n.totalOutgoingTrust;
+    }
+    console.log(cycles + ' cycles');
+    console.log('highestTotalTrust ' + highestTotalTrust);
+    return state;
+};
+
 var runRootless = function (state0, trusts) {
     state0 = JSON.parse(JSON.stringify(state0));
     trusts = dedupe(trusts);
+    var cycles = 0;
     var cycle = function () {
+        cycles++;
         var karmaByAddr = {};
         var addr;
         for (addr in state0) { karmaByAddr[addr] = state0[addr].karma; }
         var totalTrustByAddr = {};
         var highestTotal = 0;
         trusts.forEach(function (t) {
-            var total = totalTrustByAddr[t.src] = (totalTrustByAddr[t.src]|0) + t.trust;
+            var total = totalTrustByAddr[t.src] = (totalTrustByAddr[t.src]||0) + t.trust;
             if (total > highestTotal) { highestTotal = total; }
-            karmaByAddr[t.dst] = karmaByAddr[t.dst]|0;
+            karmaByAddr[t.dst] = karmaByAddr[t.dst]||0;
         });
         var nextKarmaByAddr = {};
         trusts.forEach(function (t) {
-            var ks = karmaByAddr[t.src]|0;
-            var kd = karmaByAddr[t.dest]|0;
+            var ks = karmaByAddr[t.src]||0;
+            var kd = karmaByAddr[t.dest]||0;
             var tfrac = t.trust / highestTotal;
             if (tfrac > 1) { throw new Error(); }
             nextKarmaByAddr[t.dest] = (nextKarmaByAddr[t.dest] || kd) +
@@ -124,24 +234,33 @@ var runRootless = function (state0, trusts) {
         var totalDiff = 0;
         for (addr in nextKarmaByAddr) { totalKarma += nextKarmaByAddr[addr]; }
         var multiplier = 1000/totalKarma;
-        totalKarma = 0;
         for (addr in nextKarmaByAddr) {
             var nk = nextKarmaByAddr[addr] * multiplier;
             totalDiff += Math.abs(state0[addr].karma - nk);
             state0[addr].karma = nk;
         }
+        console.log(totalKarma + '  ' + totalDiff + '  ' + cycles);
         return totalDiff;
     };
     while (cycle() > 1) {}
+    console.log(cycles + ' cycles');
     return state0;
 };
 
-var compute = module.exports.compute = function (parsed, cb) {
+var computeRootless = function (parsed, cb) {
+    let trusts = parsed.filter(function (x) { return x.command === 'itrust'; });
+    let karmas = getZeroKarmas(trusts);
+    for (let x in karmas) { if (Humans.indexOf(karmas[x].addr) !== -1) { karmas[x].karma = 1; } }
+    cb(undefined, karmasToList(runRootless1(karmas, parsed)));
+}
+
+var computeRooted = function (parsed, cb) {
     parsed = parsed.filter(function (x) { return x.command === 'itrust'; });
     var out = run(parsed);
-    for (var x in out) { out[x].karma = 1; }
     cb(undefined, karmasToList(runRootless(out, parsed)));
 };
+
+var compute = module.exports.compute = computeRooted;
 
 if (module.parent === null) {
     var input = '';
@@ -157,3 +276,5 @@ if (module.parent === null) {
         });
     });
 }
+
+}());
---
+++ mksybil.js
@@ -0,0 +1,36 @@
+var Crypto = require('crypto');
+
+var mkIp = function () {
+    var buff = Crypto.randomBytes(16).toString('hex');
+    buff = buff.replace(/([0-9a-f]{4})/g, function (all, block) { return block + ':' });
+    buff = buff.replace(/^[0-9a-f]{2}/, 'fc').slice(0, -1);
+    return buff;
+};
+
+//{"command":"itrust","src":"fca8:2dd7:4987:a9be:c8fc:34d7:5a1:4606","srcNick":"jercos","dest":"fc92:8136:dc1f:e6e0:4ef6:a6dd:7187:b85f","destNick":"cjd","trust":49,"time":1455225538309}
+
+var now = function () { return (new Date()).getTime(); };
+
+var mkEntries = function (count) {
+    var i = 0;
+    var ips = (new Array(count)).fill().map(function () {
+        return { ip: mkIp(), nick: 'bot' + i++ };
+    });
+    ips.forEach(function (ipA) {
+        ips.forEach(function (ipB) {
+            if (ipB === ipA) { return; }
+            var line = {
+                command: "itrust",
+                src: ipA.ip,
+                srcNick: ipA.nick,
+                dest: ipB.ip,
+                destNick: ipB.nick,
+                trust: 100,
+                time: now()
+            };
+            console.log(JSON.stringify(line));
+        });
+    });
+};
+
+mkEntries(2);
